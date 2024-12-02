bc79a63cf0d252d6730f957d80cef53ed6366259
jihyeChu <09strawberry@naver.com>
2024-12-02 15:04:33 +0900

    [feat] UserEntity 클래스 생성

---
+++ com.sparta.msa_exam.auth/src/main/java/com/sparta/msa_exam/auth/model/entity/UserEntity.java
@@ -0,0 +1,32 @@
+package com.sparta.msa_exam.auth.model.entity;
+
+import io.hypersistence.utils.hibernate.id.Tsid;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "users")
+public class UserEntity {
+
+    @Id @Tsid
+    @Column(name = "user_id")
+    private Long id;
+
+    @Column(name = "username", nullable = false, unique = true)
+    private String username;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Builder
+    public UserEntity(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+}
